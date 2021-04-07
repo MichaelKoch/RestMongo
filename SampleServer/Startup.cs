53283990bd5efd7f5b0f32ea652ff038db618581
Michael Koch <michael_koch@hugoboss.com>
2021-04-07 19:27:29 +0200

    first try to implement odata queries

--- SampleServer/Startup.cs
+++ SampleServer/Startup.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNet.OData.Formatter;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -5,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
 using Microsoft.OpenApi.Models;
 using MongoBase.Interfaces;
 using MongoBase.Models;
@@ -12,6 +15,7 @@ using MongoBase.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+
 using System.Threading.Tasks;
 
 namespace SampleServer
@@ -25,27 +29,41 @@ namespace SampleServer
 
         public IConfiguration Configuration { get; }
 
-        
-        
+
+
         private void AddConfigSections(IServiceCollection services)
         {
             ConnectionSettings mongoSettings = new ConnectionSettings();
             Configuration.GetSection("mongo").Bind(mongoSettings);
             services.AddSingleton<IConnectionSettings>(mongoSettings);
         }
-        
-        
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddOData();
+            services.AddMvcCore(options =>
+        {
+            foreach (var outputFormatter in options.OutputFormatters.OfType<ODataOutputFormatter>().Where(_ => _.SupportedMediaTypes.Count == 0))
+            {
+                outputFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/prs.odatatestxx-odata"));
+            }
+            foreach (var inputFormatter in options.InputFormatters.OfType<ODataInputFormatter>().Where(_ => _.SupportedMediaTypes.Count == 0))
+            {
+                inputFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/prs.odatatestxx-odata"));
+            }
+        });
+            services.AddMvc(options => options.EnableEndpointRouting = false);
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "SampleServer", Version = "v1" });
             });
             AddConfigSections(services);
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-           
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,6 +78,13 @@ namespace SampleServer
 
             app.UseRouting();
             app.UseAuthorization();
+            app.UseMvc(routeBuilder =>
+            {
+
+                routeBuilder.EnableDependencyInjection();
+                routeBuilder.Select().Filter().MaxTop(null).Count();
+
+            });
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
