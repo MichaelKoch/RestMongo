99fedea4378515d3091a511ab7a8c27e55b4b4eb
Michael Koch <Michael.Koch@cliap.de>
2021-04-29 07:22:18 +0000

    cleanup

--- MongoBase.Test/Setup.cs
+++ MongoBase.Test/Setup.cs
@@ -12,8 +12,9 @@ public class Setup
     [AssemblyInitialize]
     public static void InitializeTestRun(TestContext context)
     {
-        mongoInstance =  Mongo2Go.MongoDbRunner.Start(singleNodeReplSet: true);
-        ConfigHelper._config.ConnectionString = mongoInstance.ConnectionString;      
+        throw new System.NotSupportedException("TEST TESTING EXCEPTION");
+        mongoInstance = Mongo2Go.MongoDbRunner.Start(singleNodeReplSet: true);
+        ConfigHelper._config.ConnectionString = mongoInstance.ConnectionString;
     }
     [AssemblyCleanup]
     public static void Dispose()
